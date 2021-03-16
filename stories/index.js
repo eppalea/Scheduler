7a643a1a25e3073cfbc9c6eb16d4b344b340aa0e
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-16 16:22:00 -0700

    added a show component, imported to index.js and added to appointment stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf("Button", module)
   .addParameters({
@@ -154,4 +155,12 @@ storiesOf("Appointment", module)
     <Empty
       onAdd={action('onAdd')}
     />
+  )
+  .add("Show", () =>
+  <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action('onEdit')}
+    onDelete={action('onDelete')}
+  />
   );
