c8aaa5749779b3e835fab4fb4a872571af70acab
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-17 15:37:48 -0700

    removed hardcoded days array. imported useEffect and axios. created an effect to make a get request to /api/days using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import 'components/Appointment';
 import Appointment from "components/Appointment";
+import axios from 'axios';
 
 const appointments = [
   {
@@ -51,26 +52,18 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [day, setDay] = useState(['Monday']);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    
+    axios.get('/api/days').then(response => {
+      console.log('response:', response.data)
+      setDays([...response.data])
+    });
+  }, [])
 
   return (
     <main className="layout">
