5dcc2af4b35c4a6cf9e83b97977ad3eef608f3cb
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-16 18:48:50 -0700

    add reset and cancel functions to reset the inputs upon clicking cancel

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,16 @@ export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  function reset() {
+    setName("")
+    setInterviewer(null)
+  }
+
+  function cancel() {
+    reset()
+    props.onCancel() //find out why onCancel is invoked. where is this function.
+  }
+
   return (
 
     <main className="appointment__card appointment__card--create">
@@ -27,7 +37,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={cancel} danger>Cancel</Button>
           <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
