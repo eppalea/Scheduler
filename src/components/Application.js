8ecadfa8e99812915b7bea0a560f1a9c517b69c9
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-17 19:16:52 -0700

    readded the hardcoded appt variable. added a setDay and setDays functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,53 @@ import 'components/Appointment';
 import Appointment from "components/Appointment";
 import axios from 'axios';
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Eppa Lea Turniawan",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Chelsea McEwen",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 
 export default function Application(props) {
   // const [day, setDay] = useState(['Monday']);
@@ -12,8 +59,12 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
-    appointments: {}
+    // appointments: {}
   });
+  
+  const setDay = day => setState({...state, day });
+  // const setDays = days => setState({...state, days });
+  const setDays = days => setState( prev => ({ ...prev, days }));
 
   useEffect(() => {
     
@@ -34,8 +85,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
