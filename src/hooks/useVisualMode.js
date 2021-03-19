ffcda32c1ba9fa7f59207879fe709805a03b3341
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-18 17:15:22 -0700

    add a back function, updated transition function to include history

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,11 +5,42 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    console.log("mode:", mode)
-    console.log("newMode:", newMode)
+  const transition = (newMode, replace = false) => {
+    // console.log('mode is:', mode);
+    // console.log("transition newMode: ", newMode);
     setMode(newMode);
+    
+    if (replace) {
+      return initial;
+    } 
+
+    // when transition is called, adds new mode to our history
+    const newHistory = [...history];
+    newHistory.push(newMode);
+    // console.log("newHistory is: ", newHistory)
+    setHistory(newHistory);
   }
 
-  return { mode, transition }
+  
+  const back = () => {
+   
+    if (history.length < 2) {
+      return;
+    } 
+
+    // console.log('back history is:', history)
+    const prevMode = history[history.length - 2]
+    // console.log('prevMode is:', prevMode)
+    setMode(prevMode);
+   
+    setHistory(prev => {
+      // console.log('prev:', ...prev)
+      const newHistory = [...prev];
+      newHistory.pop();
+      // console.log('newHistory after pop:', newHistory)
+      return newHistory
+    });  
+  };  
+
+  return { mode, transition, back };
 }
