1919e19d03e87190d9aa88f0dd7318ded66007df
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-22 13:52:59 -0700

    added code to validate the interviewers props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import InterviewerListItem from 'components/InterviewerListItem';
 import 'components/InterviewerList.scss'
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
 
   let interviewerArray = props.interviewers.map((interviewer) =>
     <InterviewerListItem
@@ -21,3 +22,9 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
