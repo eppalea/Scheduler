4bf21d1d7f4f03abe7922c08eb16229209c440bf
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-22 17:37:05 -0700

    added two more tests, using mock fn and fireEvent

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setName("")
@@ -16,6 +17,15 @@ export default function Form(props) {
     props.onCancel()
   }
 
+  function validate() {
+    if (name === "") {
+      setError('Student name cannot be blank');
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+  
   return (
 
     <main className="appointment__card appointment__card--create">
@@ -31,12 +41,13 @@ export default function Form(props) {
             data-testid='student-name-input'
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
