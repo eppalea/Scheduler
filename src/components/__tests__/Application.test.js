7446d17382b509f5fe693a7e0735644b5251487c
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-22 21:09:42 -0700

    started a promise chain to fire when Tuesday is clicked

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, wait } from "@testing-library/react";
 
 import Application from "components/Application";
+import { fireEvent } from "@testing-library/react/dist";
 
 afterEach(cleanup);
+
+//should there be a describe?
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"));
+  return waitForElement(() => getByText("Monday"))
+    .then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
 });
+
+
